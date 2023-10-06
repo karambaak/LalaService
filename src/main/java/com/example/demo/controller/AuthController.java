2c4e79de87a060a31fbc9396b9095603c5494d5c
assem12345assem <assemsim@gmail.com>
2023-10-06 16:05:30 +0600

    finished: google auth for login and registration

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -2,8 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.UserDto;
 import com.example.demo.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+
     @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("userDto", new UserDto());
@@ -30,12 +33,23 @@ public class AuthController {
             return "redirect:/auth/login";
         }
         return "auth/register";
-
-
     }
 
     @GetMapping("/login")
     public String login() {
         return "/auth/login";
+    }
+
+    @GetMapping("/oauth_2")
+    public String pickRole(Model model) {
+
+        model.addAttribute("roles", userService.getRoles());
+        return "auth/google_user_role";
+    }
+
+    @PostMapping("/oauth_2")
+    public String pickRole(HttpServletRequest request, Authentication auth) {
+        userService.updateUser(request, auth);
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,13 +3,18 @@ package com.example.demo.service;
 import com.example.demo.dto.UserDto;
 import com.example.demo.entities.Role;
 import com.example.demo.entities.User;
+import com.example.demo.enums.UserType;
 import com.example.demo.repository.RoleRepository;
 import com.example.demo.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +28,7 @@ public class UserService {
     private final RoleRepository roleRepository;
 
     private final PasswordEncoder encoder;
+    private final AuthUserDetailsService service;
 
     public List<User> getAllUsers() {
         return repository.findAll();
@@ -30,31 +36,51 @@ public class UserService {
 
     public void register(UserDto userDto) {
         var u = repository.findByPhoneNumber(userDto.getPhoneNumber());
-        if(u.isEmpty()) {
-            Role role = null;
-            if (userDto.getRole().equalsIgnoreCase("role_specialist")) {
-                role = roleRepository.findByRole("ROLE_SPECIALIST");
-            }
-            if (userDto.getRole().equalsIgnoreCase("role_customer")) {
-                role = roleRepository.findByRole("ROLE_CUSTOMER");
-            }
-            if (role == null) {
-                log.warn("Invalid role name: {}", userDto.getRole());
-                throw new IllegalArgumentException("Invalid role name: " + userDto.getRole());
-            }
+        if (u.isEmpty()) {
+            Role role = defineUserRole(userDto.getRole());
+            String userType = defineUserType(userDto.getRole());
             var user = User.builder()
                     .phoneNumber(userDto.getPhoneNumber())
                     .email(userDto.getEmail())
                     .password(encoder.encode(userDto.getPassword()))
                     .userName(userDto.getUserName())
+                    .userType(userType)
                     .role(role)
                     .enabled(Boolean.TRUE)
+                    .registrationDate(LocalDateTime.now())
                     .build();
             repository.save(user);
         } else {
             log.warn("User already exists: {}", userDto.getPhoneNumber());
             throw new IllegalArgumentException("User already exists");
         }
+    }
+
+    public Role defineUserRole(String userRole) {
+        Role role = null;
+        if (userRole.equalsIgnoreCase("role_specialist")) {
+            role = roleRepository.findByRole("ROLE_SPECIALIST");
+        }
+        if (userRole.equalsIgnoreCase("role_customer")) {
+            role = roleRepository.findByRole("ROLE_CUSTOMER");
+        }
+        if (role == null) {
+            log.warn("Invalid role name: {}", userRole);
+            throw new IllegalArgumentException("Invalid role name: " + userRole);
+        }
+        return role;
+    }
+
+    public String defineUserType(String userRole) {
+        int underscoreIndex = userRole.indexOf('_');
+        String type = userRole.substring(underscoreIndex + 1);
+        UserType userType = null;
+        for (UserType t : UserType.values()) {
+            if (t.name().equalsIgnoreCase(type)) {
+                userType = t;
+            }
+        }
+        return userType.name();
     }
 
     public List<String> getRoles() {
@@ -65,5 +91,22 @@ public class UserService {
             list.add(r.getRole());
         }
         return list;
+    }
+
+
+    public void updateUser(HttpServletRequest request, Authentication auth) {
+        String selectedRole = request.getParameter("role");
+        Role role = defineUserRole(selectedRole);
+        String userType = defineUserType(selectedRole);
+        var userAuth = (OAuth2User) auth.getPrincipal();
+        String email = userAuth.getAttribute("email");
+        var user = repository.getByEmail(email);
+        if (!user.isEmpty()) {
+            User u = user.get();
+            u.setRole(role);
+            u.setUserType(userType);
+            repository.saveAndFlush(u);
+        }
+        service.loadUserByUsernameEmail(user.get().getEmail());
     }
 }
