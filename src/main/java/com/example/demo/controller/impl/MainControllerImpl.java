afbcf71a9dea41aa4cd16b83087066c40102e7ab
Muhammed Isabaev <isabaevvm@gmail.com>
2023-10-12 13:40:40 +0600

    added MainControllerImpl and method

---
+++ src/main/java/com/example/demo/controller/impl/MainControllerImpl.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller.impl;
+
+import com.example.demo.controller.MainController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+@RequiredArgsConstructor
+public class MainControllerImpl implements MainController {
+
+    @Override
+    public String mainPage(Model model) {
+        return "resume/resumes";
+    }
+}
