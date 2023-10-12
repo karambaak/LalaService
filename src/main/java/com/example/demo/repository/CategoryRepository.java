407d5289d2c7c02cbf0f74d3c192e3fddab73160
Aleksandr Krainiukov <karambaak@gmail.com>
2023-10-12 15:18:41 +0600

    created method, logic and test

--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
