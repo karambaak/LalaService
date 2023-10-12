8568557ed2a1457ad1cc9ec7491936a26049fab6
Muhammed Isabaev <isabaevvm@gmail.com>
2023-10-12 15:07:43 +0600

    added endpoint for searching in MainController

--- src/main/java/com/example/demo/controller/impl/MoviesRestControllerImpl.java
+++ src/main/java/com/example/demo/controller/impl/MoviesRestControllerImpl.java
@@ -1,10 +1,20 @@
 package com.example.demo.controller.impl;
 
 import com.example.demo.controller.MainRestController;
+import com.example.demo.dto.SpecialistDto;
+import com.example.demo.service.SpecialistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class MoviesRestControllerImpl implements MainRestController {
+    private final SpecialistService service;
+
+    @Override
+    public List<SpecialistDto> searchSpecialistByName(String name) {
+        return service.searchSpecialistByName(name);
+    }
 }
