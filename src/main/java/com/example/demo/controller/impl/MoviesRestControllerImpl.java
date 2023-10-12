bcc8feeb50dbcf21dcbf6213a75af8a6fa4c42ff
Muhammed Isabaev <isabaevvm@gmail.com>
2023-10-12 14:30:22 +0600

    added rest MainController

---
+++ src/main/java/com/example/demo/controller/impl/MoviesRestControllerImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller.impl;
+
+import com.example.demo.controller.MainRestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MoviesRestControllerImpl implements MainRestController {
+}
