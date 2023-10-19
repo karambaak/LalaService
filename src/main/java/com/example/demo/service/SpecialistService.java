ac9fe9b60755e0165cbd11f1f398478a720b45ea
Muhammed Isabaev <isabaevvm@gmail.com>
2023-10-19 21:39:53 +0600

    added method for getting specialist data

--- src/main/java/com/example/demo/service/SpecialistService.java
+++ src/main/java/com/example/demo/service/SpecialistService.java
@@ -2,12 +2,15 @@ package com.example.demo.service;
 
 import com.example.demo.dto.SpecialistDto;
 import com.example.demo.entities.Specialist;
+import com.example.demo.entities.User;
 import com.example.demo.repository.SpecialistRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,6 +23,11 @@ public class SpecialistService {
         List<Specialist> specialists = repository.searchSpecialistByCompanyNameContainingIgnoreCase(name);
 
         return specialists.stream().map(this::makeDto).collect(Collectors.toList());
+    }
+
+    public SpecialistDto getSpecialistByAuthentication(Authentication auth){
+        User user = (User) auth.getPrincipal();
+        return makeDto(repository.findByUser(user).orElseThrow(() -> new NoSuchElementException("Specialist not found")));
     }
 
     private SpecialistDto makeDto(Specialist specialist){
