f0b5d139aacdbd4a9e1d5c41cf9453355afeea4f
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-10-20 14:45:47 +0600

    Added update method for portfolio

--- src/main/java/com/example/demo/repository/PortfolioRepository.java
+++ src/main/java/com/example/demo/repository/PortfolioRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
+import java.util.Optional;
+
 public interface PortfolioRepository extends JpaRepositoryImplementation<Portfolio,Integer> {
     Page<Portfolio> findAll(Pageable pageable);
     Boolean findPortfolioByIdAndAndSpecialistId(long portfolioId,long specialistId);
+
+    Optional<Portfolio> findPortfolioById(long id);
 }
