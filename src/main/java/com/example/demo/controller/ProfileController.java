c55d362fa7eefc2990d502da913965735598b6a0
Muhammed Isabaev <isabaevvm@gmail.com>
2023-10-23 08:04:42 +0600

    added endpoint for profile

--- src/main/java/com/example/demo/controller/ProfileController.java
+++ src/main/java/com/example/demo/controller/ProfileController.java
@@ -2,11 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public interface ProfileController {
 
     @GetMapping("/profile")
-    String profile(Authentication auth);
+    String profile(Authentication auth, Model model);
 }
--- src/main/java/com/example/demo/controller/impl/ProfileControllerImpl.java
+++ src/main/java/com/example/demo/controller/impl/ProfileControllerImpl.java
@@ -1,20 +1,31 @@
 package com.example.demo.controller.impl;
 
 import com.example.demo.controller.ProfileController;
-import com.example.demo.entities.User;
+import com.example.demo.dto.UserDto;
+import com.example.demo.service.ResumeService;
+import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
 
 @RequiredArgsConstructor
 @Component
 @Slf4j
 public class ProfileControllerImpl implements ProfileController {
+    private final UserService userService;
+    private final ResumeService resumeService;
     @Override
-    public String profile(Authentication auth) {
-        User user = (User) auth.getPrincipal();
-        log.info("User role: {}", user.getRole().getRole());
+    public String profile(Authentication auth, Model model) {
+        User authUser = (User) auth.getPrincipal();
+        UserDto currentUser = userService.getUserByAuthentication(auth);
+
+        if (currentUser.getRole().equalsIgnoreCase("ROLE_SPECIALIST")){
+            model.addAttribute("resumes", resumeService.getResumesByUserId(currentUser.getId()));
+        }
+        model.addAttribute("user", currentUser);
         return "users/profile";
     }
 }
