a36a0b851c642e2c178b115751a11045b9f1f35b
Muhammed Isabaev <isabaevvm@gmail.com>
2023-10-26 16:09:38 +0600

    added rating to front

--- src/main/java/com/example/demo/controller/impl/ProfileControllerImpl.java
+++ src/main/java/com/example/demo/controller/impl/ProfileControllerImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.controller.impl;
 
 import com.example.demo.controller.ProfileController;
 import com.example.demo.dto.UserDto;
+import com.example.demo.service.RatingService;
 import com.example.demo.service.ResumeService;
 import com.example.demo.service.UserService;
 import com.example.demo.utils.QRCodeServiceImpl;
@@ -19,6 +20,9 @@ public class ProfileControllerImpl implements ProfileController {
     private final UserService userService;
     private final ResumeService resumeService;
     private final QRCodeServiceImpl qrCodeService;
+    private final RatingService ratingService;
+
+
     @Override
     public String profile(Authentication auth, Model model) {
         User authUser = (User) auth.getPrincipal();
@@ -26,6 +30,7 @@ public class ProfileControllerImpl implements ProfileController {
 
         if (currentUser.getRole().equalsIgnoreCase("ROLE_SPECIALIST")){
             model.addAttribute("resumes", resumeService.getResumesByUserId(currentUser.getId()));
+            model.addAttribute("rating", ratingService.getSpecialistRatingById(currentUser.getId()));
         }
         model.addAttribute("user", currentUser);
         model.addAttribute("qrCode", qrCodeService.generateQRCode("/favorites/add/" + currentUser.getPhoneNumber(), 500, 500));
