0d76d7f10978cbf5f9805a1f927b741ecc702f95
Aleksandr Krainiukov <karambaak@gmail.com>
2023-11-30 23:24:53 +0600

    task 205 finished

--- src/main/java/com/example/demo/utils/CountryCode.java
+++ src/main/java/com/example/demo/utils/CountryCode.java
@@ -18,7 +18,7 @@ public class CountryCode {
     public static List<CountryCode> readCountryCodesFromFile() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         List<CountryCode> countryCodes = objectMapper.readValue(
-                new File("/static/phone_number_codes.json"),
+                new File("phone_number_codes.json"),
                 new TypeReference<>() {
                 });
         return countryCodes;
