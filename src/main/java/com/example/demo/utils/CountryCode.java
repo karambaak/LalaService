7a7d48a9e2bf2e3ae5457165b3cbdb0a5027a131
Aleksandr Krainiukov <karambaak@gmail.com>
2023-12-01 16:07:32 +0600

    fixing filePath

--- src/main/java/com/example/demo/utils/CountryCode.java
+++ src/main/java/com/example/demo/utils/CountryCode.java
@@ -6,8 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Data
@@ -18,9 +18,12 @@ public class CountryCode {
     private Integer countOfNumber;
     public static List<CountryCode> readCountryCodesFromFile() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        List<CountryCode> countryCodes = objectMapper.readValue(
-                new File("src/main/resources/static/phone_number_codes.json"),
-                new TypeReference<>() {
+        InputStream inputStream = CountryCode.class.getResourceAsStream("/static/phone_number_codes.json");
+        if (inputStream == null) {
+            throw new IOException("File phone_number_codes.json not found");
+        }
+        List<CountryCode> countryCodes = objectMapper.readValue(inputStream,
+                new TypeReference<List<CountryCode>>() {
                 });
         return countryCodes;
     }
