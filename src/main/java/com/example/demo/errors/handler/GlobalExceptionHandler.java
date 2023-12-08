7480f8ef88e7169a57f23cd64524a9b1838fcf87
Aleksandr Krainiukov <karambaak@gmail.com>
2023-12-08 16:21:21 +0600

    added superman

--- src/main/java/com/example/demo/errors/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/errors/handler/GlobalExceptionHandler.java
@@ -1,130 +1,117 @@
 package com.example.demo.errors.handler;
 
-import com.example.demo.errors.ResourceNotFoundException;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.servlet.NoHandlerFoundException;
-
-import java.nio.file.AccessDeniedException;
-import java.util.NoSuchElementException;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NoHandlerFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNoHandlerFoundException(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.NOT_FOUND.value());
-        model.addAttribute("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
-        model.addAttribute("details", request.getRequestURI());
-        return "errors/error";
-    }
-    @ExceptionHandler(HttpClientErrorException.NotFound.class)
-    private String notFound(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.NOT_FOUND.value());
-        model.addAttribute("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    private String illegalArgument(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
-        model.addAttribute("reason", HttpStatus.BAD_REQUEST.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-
-    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
-    private String badRequest(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
-        model.addAttribute("reason", HttpStatus.BAD_REQUEST.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-    @ExceptionHandler(NoSuchElementException.class)
-    private String noSuchElement(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
-        model.addAttribute("reason", HttpStatus.BAD_REQUEST.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-    @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
-    private String unauthorized(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.UNAUTHORIZED.value());
-        model.addAttribute("reason", HttpStatus.UNAUTHORIZED.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-
-    @ExceptionHandler(AccessDeniedException.class)
-    private String accessDenied(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.FORBIDDEN.value());
-        model.addAttribute("reason", HttpStatus.FORBIDDEN.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-
-    @ExceptionHandler(HttpClientErrorException.Forbidden.class)
-    private String forbidden(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.FORBIDDEN.value());
-        model.addAttribute("reason", HttpStatus.FORBIDDEN.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    private String pageNotFound(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.NOT_FOUND.value());
-        model.addAttribute("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-
-    @ExceptionHandler(Exception.class)
-    private String otherException(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.FORBIDDEN.value());
-        model.addAttribute("reason", HttpStatus.FORBIDDEN.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-
-    @ExceptionHandler(HttpClientErrorException.MethodNotAllowed.class)
-    private String methodNotAllowed(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.METHOD_NOT_ALLOWED.value());
-        model.addAttribute("reason", HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-
-    @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
-    private String internalServerError(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        model.addAttribute("reason", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-
-    @ExceptionHandler(HttpClientErrorException.UnsupportedMediaType.class)
-    private String unsupportedMediaType(HttpServletRequest request, Model model) {
-        model.addAttribute("status", HttpStatus.UNSUPPORTED_MEDIA_TYPE.value());
-        model.addAttribute("reason", HttpStatus.UNSUPPORTED_MEDIA_TYPE.getReasonPhrase());
-        model.addAttribute("details", request);
-        return "errors/error";
-    }
-    @ExceptionHandler(UsernameNotFoundException.class)
-    private String handleUsernameNotFoundException(HttpServletRequest request, Model model, UsernameNotFoundException ex) {
-        model.addAttribute("status", HttpStatus.NOT_FOUND.value());
-        model.addAttribute("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
-        model.addAttribute("details", ex.getMessage());
-        return "errors/error";
-    }
+//    @ExceptionHandler(NoHandlerFoundException.class)
+//    @ResponseStatus(HttpStatus.NOT_FOUND)
+//    public String handleNoHandlerFoundException(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.NOT_FOUND.value());
+//        model.addAttribute("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
+//        model.addAttribute("details", request.getRequestURI());
+//        return "errors/error";
+//    }
+//    @ExceptionHandler(HttpClientErrorException.NotFound.class)
+//    private String notFound(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.NOT_FOUND.value());
+//        model.addAttribute("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//
+//    @ExceptionHandler(IllegalArgumentException.class)
+//    private String illegalArgument(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
+//        model.addAttribute("reason", HttpStatus.BAD_REQUEST.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//
+//    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
+//    private String badRequest(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
+//        model.addAttribute("reason", HttpStatus.BAD_REQUEST.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//    @ExceptionHandler(NoSuchElementException.class)
+//    private String noSuchElement(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
+//        model.addAttribute("reason", HttpStatus.BAD_REQUEST.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//    @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
+//    private String unauthorized(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.UNAUTHORIZED.value());
+//        model.addAttribute("reason", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//
+//    @ExceptionHandler(AccessDeniedException.class)
+//    private String accessDenied(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.FORBIDDEN.value());
+//        model.addAttribute("reason", HttpStatus.FORBIDDEN.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//
+//    @ExceptionHandler(HttpClientErrorException.Forbidden.class)
+//    private String forbidden(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.FORBIDDEN.value());
+//        model.addAttribute("reason", HttpStatus.FORBIDDEN.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    private String pageNotFound(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.NOT_FOUND.value());
+//        model.addAttribute("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//
+//    @ExceptionHandler(Exception.class)
+//    private String otherException(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.FORBIDDEN.value());
+//        model.addAttribute("reason", HttpStatus.FORBIDDEN.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//
+//    @ExceptionHandler(HttpClientErrorException.MethodNotAllowed.class)
+//    private String methodNotAllowed(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.METHOD_NOT_ALLOWED.value());
+//        model.addAttribute("reason", HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//
+//    @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
+//    private String internalServerError(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+//        model.addAttribute("reason", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//
+//    @ExceptionHandler(HttpClientErrorException.UnsupportedMediaType.class)
+//    private String unsupportedMediaType(HttpServletRequest request, Model model) {
+//        model.addAttribute("status", HttpStatus.UNSUPPORTED_MEDIA_TYPE.value());
+//        model.addAttribute("reason", HttpStatus.UNSUPPORTED_MEDIA_TYPE.getReasonPhrase());
+//        model.addAttribute("details", request);
+//        return "errors/error";
+//    }
+//    @ExceptionHandler(UsernameNotFoundException.class)
+//    private String handleUsernameNotFoundException(HttpServletRequest request, Model model, UsernameNotFoundException ex) {
+//        model.addAttribute("status", HttpStatus.NOT_FOUND.value());
+//        model.addAttribute("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
+//        model.addAttribute("details", ex.getMessage());
+//        return "errors/error";
+//    }
 }
